58e33f8512f126c6dfcdafadcb0af045cd89d466
rini3 <ri.ni03dabral@gmail.com>
2024-07-26 07:19:37 +0530

    schema added

---
+++ sec2/accounts/src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `Customer` (
+                                          `customerId` int AUTO_INCREMENT  PRIMARY KEY,
+                                          `name` varchar(100) NOT NULL,
+    `email` varchar(100) NOT NULL,
+    `mobileNumber` varchar(20) NOT NULL,
+    `createdAt` date NOT NULL,
+    `createdBy` varchar(20) NOT NULL,
+    `updatedAt` date DEFAULT NULL,
+    `updatedBy` varchar(20) DEFAULT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+                                          `customer_id` int NOT NULL,
+                                          `account_number` int AUTO_INCREMENT  PRIMARY KEY,
+                                          `account_type` varchar(100) NOT NULL,
+    `branch_address` varchar(200) NOT NULL,
+    `createdAt` date NOT NULL,
+    `createdBy` varchar(20) NOT NULL,
+    `updatedAt` date DEFAULT NULL,
+    `updatedBy` varchar(20) DEFAULT NULL
+    );
