d0c5fe6b433f37bb1bf9116aef86386ce92a5ac2
rini3 <ri.ni03dabral@gmail.com>
2024-09-30 09:41:31 +0530

    spring cloud gateway created

---
+++ sec9/gatewayserver/src/test/java/com/edemoProjects/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edemoProjects.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
